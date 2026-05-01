427d3dcf299e739f2d6456c78c2ac5fc02b84112
MYST-Drakonoid <107299018+MYST-Drakonoid@users.noreply.github.com>
2026-05-01 10:39:59 -0700

    setup

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.send(`Hello, ${name}!`); // <-- UPDATED
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
