c03b0b52c4526428c7fc4994d21d49210c3595f8
MYST-Drakonoid <107299018+MYST-Drakonoid@users.noreply.github.com>
2026-05-01 10:52:37 -0700

    setup finished

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
 import express from 'express';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const app = express();
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+/**
+ * Routes
+ */
+app.get('/', (req, res) => {
+    const title = 'Welcome Home';
+    res.render('home', { title });
+});
+
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', { title });
+});
+
+app.get('/products', (req, res) => {
+    const title = 'Our Products';
+    res.render('products', { title });
+});
 
 const name = process.env.NAME; // <-- NEW
 
-app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
-});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
